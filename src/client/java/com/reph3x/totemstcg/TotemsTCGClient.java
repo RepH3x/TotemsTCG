e7cb95ef63882ca061323c6c49e66bf408a8ab26
RepH3x <lewissamuala@gmail.com>
2025-01-19 22:42:09 -0800

    Original Mod Example

---
+++ src/client/java/com/reph3x/totemstcg/TotemsTCGClient.java
@@ -0,0 +1,10 @@
+package com.reph3x.totemstcg;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TotemsTCGClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
